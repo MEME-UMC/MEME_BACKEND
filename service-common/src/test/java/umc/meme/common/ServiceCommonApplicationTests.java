adb56b046d2cab8bc3cc7b4d607760baf88d9ba8
Jaeyoung Lim <limjyjustin@gmail.com>
2024-06-02 16:29:53 +0900

    add: service-common 모듈 생성
    Lombok 의존성만 추가

---
+++ service-common/src/test/java/umc/meme/common/ServiceCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.meme.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
